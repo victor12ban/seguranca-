c6a8ba237d946999d72612a05d42dd8bd70afc96
victor <vp050219@gmail.com>
2016-05-06 16:36:51 -0300

    victor aula do dia 06.05

---
+++ aula0605/index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+   <title>formulario</title>
+  <meta> http-equiv="Content-Type" content="text/html"; charset= isso-8859-1">
+  </head>
+   <body>
+      <form name="form1" method="post" action="enviar.php"
+     <p> login:
+     <imput nome="login" type"text" id="login">
+     </p>
+      <p>senha
+       <imput nome"senha" type"possword" id"senha">
+     </p>
+      <p>
+      <imput type="subimit" name="Submit" value="Enviar"
+          </p>
+          </form>
+         </body>
+       <html> 
